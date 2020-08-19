146d3347bb653d1e301c4e3e23212621871981aa
yeonu <dusdn9464@hangilcnc.com>
2020-08-19 15:55:29 +0900

    1. 설명 UI

---
+++ Practice/Assets/Scripts/BoxLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class BoxLine : MonoBehaviour
+{
+    LineRenderer lr;
+    Vector3 exBoxPos, conBoxPos;
+    public float lineSize = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.startWidth = lineSize;
+        lr.endWidth = lineSize;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.hasChanged)
+        {
+            exBoxPos = gameObject.GetComponent<Transform>().position;
+            transform.hasChanged = false;
+        }
+
+        lr.SetPosition(0, exBoxPos);
+        lr.SetPosition(1, GameObject.Find("Engine").GetComponent<Transform>().position);
+    }
+}
---
+++ Practice/Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WindowsMR;
+
+public class ClickEvent : MonoBehaviour
+{
+    public WindowsMRGestures gestures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
